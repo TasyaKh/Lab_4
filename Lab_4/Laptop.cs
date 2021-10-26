36368245149647896635e02230c5178069ab701a
tasya132002@gmail.com <tasya132002@gmail.com>
2021-10-26 12:41:38 +0800

    Доработали класс

--- Lab_4/Laptop.cs
+++ Lab_4/Laptop.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace Lab_4
 {
-    class Laptop : Technique
+    class Laptop : Technique            //Класс ноутбуков, наследует класс техника
     {
         private bool backlightKeyboard; //С подстветкой клавиатура или нет
         private byte numberCores;       //Число ядер
         private int spaceHardDrive;     //Объем памяти жесткого диска гб
         public Laptop()
         {
-            backlightKeyboard = rnd.Next() % 2 == 1 ? true : false;
-            numberCores = (byte)(rnd.Next() % 32 + 1);
-            spaceHardDrive = (rnd.Next() % 1000 + 1);
+            backlightKeyboard = rnd.Next() % 2 == 1 ? true : false;  //0-1
+            numberCores = (byte)(rnd.Next() % 32 + 1);               //1-32
+            spaceHardDrive = (rnd.Next() % 1000 + 10);               //10-1000
 
             sizeOfDisplay(10, 18);
         }
@@ -25,7 +25,7 @@ namespace Lab_4
             str += base.getInfo();
             str += String.Format("\nПодсветка на клавиатуре: " + backlightKeyboard);
             str += String.Format("\nЧисло ядер: " + numberCores);
-            str += String.Format("\nОбъем жесткого диска: " + spaceHardDrive + " гб.");
+            str += String.Format("\nЖесткий диск: " + spaceHardDrive + " гб.");
 
             return str;
         }
